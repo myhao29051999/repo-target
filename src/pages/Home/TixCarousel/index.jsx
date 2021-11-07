7e536a41e68e1fa4c39b74425cb352f730e89b61
myhao29051999 <55449729+myhao29051999@users.noreply.github.com>
2021-11-07 14:42:08 +0700

    update

--- src/pages/Home/TixCarousel/index.jsx
+++ src/pages/Home/TixCarousel/index.jsx
@@ -1,3 +1,4 @@
+import { SELECTION_NONE } from "antd/lib/table/hooks/useSelection";
 import React from "react";
 import { Carousel } from "react-carousel-minimal";
 
@@ -8,54 +9,26 @@ export default function TixCarousel() {
   const data = [
     {
       image:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/GoldenGateBridge-001.jpg/1200px-GoldenGateBridge-001.jpg",
-      caption: "San Francisco",
+        "https://www.bhdstar.vn/wp-content/uploads/2018/03/Teaser-TroChoi-DayBinh.png",
     },
     {
       image:
-        "https://cdn.britannica.com/s:800x450,c:crop/35/204435-138-2F2B745A/Time-lapse-hyper-lapse-Isle-Skye-Scotland.jpg",
-      caption: "Scotland",
+        "https://www.bhdstar.vn/wp-content/uploads/2018/03/Black-Widow.png",
+    },
+    {
+      image: " https://www.bhdstar.vn/wp-content/uploads/2018/03/Shangchi.png",
     },
     {
       image:
-        "https://static2.tripoto.com/media/filter/tst/img/735873/TripDocument/1537686560_1537686557954.jpg",
-      caption: "Darjeeling",
-    },
-    {
-      image:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/1/16/Palace_of_Fine_Arts_%2816794p%29.jpg/1200px-Palace_of_Fine_Arts_%2816794p%29.jpg",
-      caption: "San Francisco",
-    },
-    {
-      image:
-        "https://i.natgeofe.com/n/f7732389-a045-402c-bf39-cb4eda39e786/scotland_travel_4x3.jpg",
-      caption: "Scotland",
-    },
-    {
-      image:
-        "https://www.tusktravel.com/blog/wp-content/uploads/2020/07/Best-Time-to-Visit-Darjeeling-for-Honeymoon.jpg",
-      caption: "Darjeeling",
-    },
-    {
-      image:
-        "https://www.omm.com/~/media/images/site/locations/san_francisco_780x520px.ashx",
-      caption: "San Francisco",
-    },
-    {
-      image:
-        "https://images.ctfassets.net/bth3mlrehms2/6Ypj2Qd3m3jQk6ygmpsNAM/61d2f8cb9f939beed918971b9bc59bcd/Scotland.jpg?w=750&h=422&fl=progressive&q=50&fm=jpg",
-      caption: "Scotland",
-    },
-    {
-      image:
-        "https://www.oyorooms.com/travel-guide/wp-content/uploads/2019/02/summer-7.jpg",
-      caption: "Darjeeling",
+        "https://www.bhdstar.vn/wp-content/uploads/2021/10/Venom-1920x1080.jpg",
     },
   ];
   const captionStyle = {
-    fontSize: "2em",
-    fontWeight: "bold",
+    // fontSize: "2em",
+    // fontWeight: "bold",
+    display: "none",
   };
+
   const slideNumberStyle = {
     fontSize: "20px",
     fontWeight: "bold",
@@ -63,17 +36,18 @@ export default function TixCarousel() {
   return (
     <TixCarouselStyle className="App">
       <div style={{ textAlign: "center" }}>
-        <div>
+        <div className="tix-carousel">
           <Carousel
             data={data}
-            time={2000}
+            time={3000}
             width="100%"
-            height="620px"
+            height="650px"
             captionStyle={captionStyle}
             radius="0px"
             slideNumber={false}
             slideNumberStyle={slideNumberStyle}
             captionPosition="center"
+            caption={false}
             automatic={true}
             dots={false}
             pauseIconColor="white"
@@ -85,10 +59,13 @@ export default function TixCarousel() {
             style={{
               textAlign: "center",
               maxWidth: "100%",
-              maxHeight: "620px",
+              maxHeight: "650px",
               margin: "60px auto",
             }}
           />
+          <div className="button-play">
+            <i className="fa fa-play"></i>
+          </div>
         </div>
       </div>
     </TixCarouselStyle>
--- src/pages/Home/TixCarousel/style.jsx
+++ src/pages/Home/TixCarousel/style.jsx
@@ -1,7 +1,57 @@
 import styled from "styled-components";
+import variableStyles from "styles/variable-styles";
 
 export const TixCarouselStyle = styled.div`
+  position: relative;
+
   #thumbnail-div {
     justify-content: center;
+  }
+
+  .prev,
+  .next {
+    cursor: pointer;
+    position: absolute;
+    top: 49%;
+    width: auto;
+    padding: 16px;
+    margin-top: -22 px;
+    color: white;
+    font-weight: bold;
+    font-size: 18px;
+    transition: 0.6s ease;
+    border-radius: 0 3 px 3 px 0;
+    -webkit-user-select: none;
+    user-select: none;
+  }
+  .tix-carousel {
+    &:hover {
+      .button-play {
+        display: block;
+      }
+    }
+  }
+  .button-play {
+    position: absolute;
+    background: rgba(149, 145, 145, 0.855);
+    font-size: 60px;
+    bottom: 40%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    cursor: pointer;
+    display: none;
+    transition: all 1s;
+    height: 70px;
+    width: 70px;
+    border-radius: 50%;
+    border: 1px solid ${variableStyles.textWhite};
+    i {
+      font-size: ${variableStyles.fontSize_24};
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      color: ${variableStyles.textWhite};
+    }
   }
 `;
